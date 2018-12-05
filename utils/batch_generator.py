669b94326eead96cf09cdebde8a955a8a71f3021
Xiwen Li <xiwenli@wustl.edu>
2018-12-05 03:12:27 -0600

    Upload Tets

--- utils/batch_generator.py
+++ utils/batch_generator.py
@@ -7,10 +7,12 @@ import os
 
 _cur_dir = os.path.dirname(os.path.realpath(__file__))
 TRAIN_DIR = os.path.join(_cur_dir, '../data/train/')
+TEST_DIR = os.path.join(_cur_dir, '../data/test/')
 
 def build_dict():
     v_dict={}
     d_dict={}
+    count=0
     for i, dirname in enumerate(os.listdir(TRAIN_DIR)):
         rel_name=TRAIN_DIR+dirname
         d_dict[i]=dirname
@@ -19,6 +21,17 @@ def build_dict():
             c_dict[j]=filename
         v_dict[dirname]=c_dict
     return v_dict, d_dict
+
+def build_dict_test():
+    td_dict={}
+    count=0
+    for filename in os.listdir(TEST_DIR):
+        name_cate=filename.rsplit("_",1)[0]
+        if name_cate in td_dict:
+            td_dict[name_cate].append(filename)
+        else:
+            td_dict[name_cate]=[filename]
+    return td_dict
 
 class basicIterator:
 
@@ -99,4 +112,28 @@ class ImprovedTriplessIterator:
             img_tar=resize(np.float32(imread(fn(TRAIN_DIR+ self.d_dict[idx[1]] + '/' + self.visited[self.d_dict[idx[1]]][tar_idx])))/255.,(224,224,3),anti_aliasing=True)
             res_x.append([img_src,img_src,img_tar])
             res_y.append([self.d_dict[idx[0]],self.d_dict[idx[0]],self.d_dict[idx[1]]])
+        return np.array(res_x),np.array(res_y)
+
+class ImprovedTriplessIteratorTest:
+
+    def __init__(self, batch_size):
+        self.td_dict=build_dict_test()
+        _,self.d_dict=build_dict()
+        self.num_cate=3
+        self.cate_size=batch_size/self.num_cate
+
+    def next(self):
+        res_x=[]
+        res_y=[]
+        for i in range(int(self.cate_size)):
+            idx=np.random.randint(102, size=2)
+            src_name=self.d_dict[idx[0]]
+            tar_name=self.d_dict[idx[1]]
+            self.td_dict[src_name]
+            src_idx=np.random.randint(len(self.td_dict[src_name]))
+            tar_idx=np.random.randint(len(self.td_dict[tar_name]))
+            img_src = resize(np.float32(imread(fn(TEST_DIR + '/' + self.td_dict[src_name][src_idx])))/255.,(224,224,3),anti_aliasing=True)
+            img_tar = resize(np.float32(imread(fn(TEST_DIR + '/' + self.td_dict[tar_name][tar_idx])))/255.,(224,224,3),anti_aliasing=True)
+            res_x.append([img_src,img_src,img_tar])
+            res_y.append([src_name,src_name,tar_name])
         return np.array(res_x),np.array(res_y)
